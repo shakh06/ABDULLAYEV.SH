6653caf57d24ecbffe0cda6e6ae400d6cf1c4a93
shakh06 <abdullayevmuhammad0037@gmail.com>
2022-04-27 15:07:47 +0300

    Initial commit

---
+++ 13dars.py
@@ -0,0 +1,128 @@
+# phyton_words = {
+#     "integer":"butun son",
+#     "float":"onlik son",
+#     "boolen":"mantiqiy qiymat",
+#     "for":"bir amalni qayta qayta bajarish",
+#     "if":"shartlarni tekshirish aperatori",
+#     "title":"hamma bosh harflarni kattartirib beradi", 
+#     "capitalize":"gap boshidagi yo soz boshidagi sozni bosh harfini kattartiradi"
+#     }
+# for key, value in sorted(phyton_words.items()):
+#     print(f"{key.title()} - {value}")
+
+
+# davlatlar = {
+#     "ozbekiston":"toshkent",
+#     "aqsh":"washington",
+#     "rossiya":"moskiva",
+#     "tojikiston":"dushanbe",
+#     "qirgiziston":"bishkek",
+#     "qozogiston":"nursulton",
+#     "singapur":"singapur"
+#     }
+# country = input("qaysi davlat poytaxtini bilishni hohlaysiz?:").lower()
+# capital = davlatlar.get(country)
+# if capital==None:
+#     print("kechirasiz, bizda bu haqida malumot yoq")
+# else:
+#     print(f"{country.upper()}ning poytaxti {capital.title()} shahri")
+
+
+# menu = {
+#         "lagmon":15000,
+#         "osh":10000,
+#         "non":2000,
+#         "somsa":4000,
+#         "shashlik":70000
+    
+#         }
+# print("3ta taom buyurtma qiling.")
+# buyurtmalar = []
+# for a in range(3):
+#     buyurtmalar.append(input(f"{a+1}-taom:").lower())
+
+# for buyurtma in buyurtmalar:
+#     if buyurtma in menu:
+#        print(f"{buyurtma.title()} {menu[buyurtma]}sum")
+#     else:
+#        print(f"kechirasiz, bizda {buyurtma} yoq")
+
+# words={
+#       "phone":"telefon",
+#       "board":"doska",
+#       "lamp":"chiroq",
+#       "bag":"sumka",
+#       "hair":"soch",
+#       "book":"kitob",
+#       "copybook":"daftar",
+#       "animal":"hayvon",
+#       "lion":"sher",
+#       "dog":"kuchuk"
+#       }
+# for key, value in sorted(words.items()):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 14DARS.py
@@ -0,0 +1,117 @@
+# print("kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "istalgan son kiriting"
+# savol+="(dasturnoi toxtashish uchun 'exit' deb yozing):"
+# qiymat = ''
+# while qiymat !='exit':
+#     qiymat = input(savol)
+#     if qiymat !='exit':
+#         print(float(qiymat)**2)
+# print('dastur tugadi')
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     if son ==5:
+#        break  
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     if son ==5:
+#        continue
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+
+
+# son = 0
+# while son<15:
+#     son += 1
+#     if son%2==1:
+#       continue
+#     else:
+#           print(son)
+
+
+# import math
+# print("Kiritilgan sonning ildizini qaytaruvchi dastur.")
+# savol = "Istalgan son kiriting"
+# savol+="(dasturni toxtashish uchun 'exit' deb yozing):"
+# while True:
+#      son =(input(savol))
+#      if son == 'exit':
+#         break
+#      else:
+#           print(math.sqrt(float(son)))
+# print('Dastur to\'xtatildi!')
+
+# son = 0
+# while son<10:
+#     son += 1
+#     if son%2==0:
+#       continue
+#     else:
+#           print(son)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
